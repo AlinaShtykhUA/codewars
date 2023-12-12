56077f1dfd0c56d27d02c680a54cd09b94a6b5e6
Alina Shtykh <alinashtykh@gmail.com>
2023-12-12 14:51:27 +0100

    day 21

--- codewars.js
+++ codewars.js
@@ -498,3 +498,22 @@ function reverseLetter(str) {
     .join('')
     .replace(/[^a-zA-Z]/g, '');
 }
+
+//* Day 21 12/12
+//todo Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+//! Моє рішення:
+function comp(array1, array2) {
+  if (!array1 || !array2) {
+    return false;
+  }
+  array1.sort((x, y) => x - y);
+  array2.sort((x, y) => x - y);
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] * array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
