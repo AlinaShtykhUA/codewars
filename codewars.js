3d72c23ac3bb2fc10528d7d498a2a6e8b4593c02
Alina Shtykh <alinashtykh@gmail.com>
2023-12-19 22:02:58 +0100

    day 28

--- codewars.js
+++ codewars.js
@@ -589,3 +589,38 @@ function stray(numbers) {
 function roundToNext5(n) {
   return Math.ceil(n / 5) * 5;
 }
+
+//* Day 28 19/12
+//todo Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+//todo The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+//todo Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+// Example:
+//   Let P be the Principal = 1000.00
+//   Let I be the Interest Rate = 0.05
+//   Let T be the Tax Rate = 0.18
+//   Let D be the Desired Sum = 1100.00
+// After 1st Year -->
+//   P = 1041.00
+// After 2nd Year -->
+//   P = 1083.86
+// After 3rd Year -->
+//   P = 1128.30
+//todo Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+//todo Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+//todo Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+//! Моє рішення:
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
+
+  while (principal < desired) {
+    const interestEarned = principal * interest;
+
+    const netInterest = interestEarned * (1 - tax);
+
+    principal += netInterest;
+
+    years++;
+  }
+
+  return years;
+}
