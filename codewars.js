37d42be91ab6115a0e7c118e3b1b6ba63a38adee
Alina Shtykh <alinashtykh@gmail.com>
2023-12-27 17:45:14 +0100

    day 36(2)

--- codewars.js
+++ codewars.js
@@ -751,3 +751,35 @@ function descendingOrder(n) {
       .join('')
   );
 }
+
+//todo In this kata, we want to convert a URL query string into a nested object. The query string will contain parameters that may or may not have embedded dots ('.'), and these dots will be used to break up the properties into the nested object. You will receive a string input that looks something like this:
+//user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue
+// Your method should return an object hash-map that looks like this:
+//
+// {
+//   'user': {
+//     'name': {
+//       'firstname': 'Bob',
+//       'lastname': 'Smith'
+//     },
+//     'favoritecolor': 'Light Blue'
+//   }
+// }
+//todo You can expect valid input. You won't see input like:
+// This will NOT happen
+// foo=1&foo.bar=2
+//todo All properties and values will be strings — and the values should be left as strings to pass the tests. Make sure you decode the URI components correctly
+//! Моє рішення:
+function convertQueryToMap(query) {
+  return query.split('&').reduce((result, pair) => {
+    const [key, value] = pair.split('=').map(decodeURIComponent);
+
+    key.split('.').reduce((nested, prop, index, array) => {
+      return (
+        nested[prop] || (nested[prop] = index === array.length - 1 ? value : {})
+      );
+    }, result);
+
+    return result;
+  }, {});
+}
